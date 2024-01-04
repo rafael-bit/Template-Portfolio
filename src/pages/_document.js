90508d211749d3f256a50598d128604689ce0be9
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-01-04 15:09:17 -0300

    Finish Header

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <title>Portfolio</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
